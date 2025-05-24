3abfd17a4d3ddfcf2903a10ad8e22f00e2ce6b61
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2025-05-24 11:49:12 -0300

    Add files via upload

---
+++ CustomUserdetails.java
@@ -0,0 +1,55 @@
+package com.example.demo.Utilitarios;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserdetails implements UserDetails{
+    private final String username;
+    private final String email;
+
+    public CustomUserdetails(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
