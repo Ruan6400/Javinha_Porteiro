5a943fc75e4f5e7a1043de3d30ff4871fcaaf534
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2025-05-24 00:12:57 -0300

    Add files via upload

--- MainController.java
+++ MainController.java
@@ -2,6 +2,9 @@ package com.tavolaquad.porteiro.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.tavolaquad.porteiro.Utilitarios.JwtUtil;
+
+
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 public class MainController {
@@ -10,9 +13,9 @@ public class MainController {
         return "Rota protegida";
     }
 
-    
+
     @GetMapping("/public/rota")
     public String publicRoute() {
-        return "Rota pública";
+        return JwtUtil.generateToken("jão","jão@email" );
     }
 }
